673455d9282624b0a6e322091ccfbd02b2656baf
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-02-13 22:51:32 -0300

    Final

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # TrailerFlix
- 
+> Aplicativo para desenvolvimento Swift/iOS
+
+* Linguagem
+    * Swift 5.5
+    * MVC
+    
+* Auto Layout
+    * Autoresizing
+    * Constraint
+    * Assets
+    
+* Components
+    * UIView
+    * UILabel
+    * UIImage
+    * UITableView
+    * UITableViewCell
+    * UIButton
+    * AVPlayer
+    * AVPlayerViewController
+    * Segue
+    
+Recursos
+    * Text Attributed
+    * Content Hugging Priority
+    
+## Snapshots
+
+<img src="1.png" width="200">  
+<br/>
+
+<img src="2.png" width="200"> 
+<br/> 
--- TrailerFlix/TrailerFlix/TrailerViewController.swift
+++ TrailerFlix/TrailerFlix/TrailerViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class TrailerViewController: UIViewController {
     
@@ -16,10 +17,39 @@ class TrailerViewController: UIViewController {
     @IBOutlet weak var viTrailer: UIView!
     
     var trailer: Trailer!
-    
+    var player: AVPlayer!
+    var playerController: AVPlayerViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        prepareView()
+        preparePlayer()
+    }
+    
+    func prepareView() {
+        lbTitle.text = trailer.title
+        lbYear.text = "\(trailer.year)"
+        var rating = "Ainda não avaliado"
+        if trailer.rating > 0 {
+            rating = ""
+            for _ in 1...trailer.rating {
+                rating += "⭐️"
+            }
+        }
+        lbRating.text = rating
+        ivTrailer.image = UIImage(named: trailer.poster + "-large")
+    }
+    
+    func preparePlayer() {
+        let url = URL(fileURLWithPath: trailer.url)
+        player = AVPlayer(url: url)
+        playerController = AVPlayerViewController()
+        playerController.player = player
+        playerController.showsPlaybackControls = true
+        playerController.player?.play()
+        playerController.view.frame = viTrailer.bounds
+        viTrailer.addSubview(playerController.view)
     }
 
     @IBAction func close(_ sender: UIButton) {
